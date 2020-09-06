2d82ff6785ae293a88c5297f989fa6118f5f154b
Vincent Lohse <vincent.lohse@live.de>
2020-09-06 15:39:00 +0200

    Removed entrypoint from Dockerfile-entrypoint

--- lab1/entrypoint.sh
+++ lab1/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "Starting Hadoop"
+# For some reason does not work if run as actual Docker entrypoint
+
+echo "Starting Hadoop namenode"
 $HADOOP_HOME/bin/hdfs --daemon start namenode
+echo "Starting Hadoop datanode"
 $HADOOP_HOME/bin/hdfs --daemon start datanode
 
 echo "Starting HBase"
