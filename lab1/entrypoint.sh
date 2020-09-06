a256234414c13cc5c2324d3e39bbc54123e435dd
Vincent Lohse <vincent.lohse@live.de>
2020-09-06 23:58:57 +0200

    Added key generation to Dockerfile + starting ssh server in entrypoint

--- lab1/entrypoint.sh
+++ lab1/entrypoint.sh
@@ -7,5 +7,14 @@ $HADOOP_HOME/bin/hdfs --daemon start namenode
 echo "Starting Hadoop datanode"
 $HADOOP_HOME/bin/hdfs --daemon start datanode
 
+# echo "Generate ssh key without password"
+# ssh-keygen -t rsa -P ""
+
+# echo "Copy id_rsa.pub to authorized-keys"
+# cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys
+
+echo "Starting the ssh server"
+/etc/init.d/ssh start
+
 echo "Starting HBase"
-$HBASE_HOME/bin/start-hbase.sh
+yes | $HBASE_HOME/bin/start-hbase.sh
