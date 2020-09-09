2a590cb7df0f1747fec4f55d5a1f44e602b5dedf
Vincent Lohse <vincent.lohse@live.de>
2020-09-09 18:36:53 +0200

    Added folder id2221 again

--- lab1/entrypoint.sh
+++ lab1/entrypoint.sh
@@ -7,12 +7,6 @@ $HADOOP_HOME/bin/hdfs --daemon start namenode
 echo "Starting Hadoop datanode"
 $HADOOP_HOME/bin/hdfs --daemon start datanode
 
-# echo "Generate ssh key without password"
-# ssh-keygen -t rsa -P ""
-
-# echo "Copy id_rsa.pub to authorized-keys"
-# cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys
-
 echo "Starting the ssh server"
 /etc/init.d/ssh start
 
