08ee5b945935d0f45a58652d652f521653c27325
Vincent Lohse <vincent.lohse@live.de>
2020-09-13 17:38:40 +0200

    Formatted example code

--- lab1/src/id2221/hbase/HBaseMapReduce.java
+++ lab1/src/id2221/hbase/HBaseMapReduce.java
@@ -18,66 +18,69 @@ import org.apache.hadoop.hbase.filter.FirstKeyOnlyFilter;
 
 public class HBaseMapReduce {
     public static class hbaseMapper extends TableMapper<Text, IntWritable> {
-	public void map(ImmutableBytesWritable rowKey, Result columns, Context context) throws IOException, InterruptedException {
-	    try {
-		// get rowKey and convert it to string
-		String inKey = new String(rowKey.get());
+        public void map(ImmutableBytesWritable rowKey, Result columns, Context context)
+                throws IOException, InterruptedException {
+            try {
+                // get rowKey and convert it to string
+                String inKey = new String(rowKey.get());
 
-		// set new key having only date
-		String oKey = inKey.split("#")[0];
+                // set new key having only date
+                String oKey = inKey.split("#")[0];
 
-		// get sales column in byte format first and then convert it to string (as it is stored as string from hbase shell)
-		byte[] bSales = columns.getValue(Bytes.toBytes("cf"), Bytes.toBytes("sales"));
-		String sSales = new String(bSales);
-		Integer sales = new Integer(sSales);
+                // get sales column in byte format first and then convert it to string (as it is
+                // stored as string from hbase shell)
+                byte[] bSales = columns.getValue(Bytes.toBytes("cf"), Bytes.toBytes("sales"));
+                String sSales = new String(bSales);
+                Integer sales = new Integer(sSales);
 
-		// emit date and sales values
-		context.write(new Text(oKey), new IntWritable(sales));
-	    } catch (RuntimeException e) {
-		e.printStackTrace();
-	    }
-	}
+                // emit date and sales values
+                context.write(new Text(oKey), new IntWritable(sales));
+            } catch (RuntimeException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static class hbaseReducer extends TableReducer<Text, IntWritable, ImmutableBytesWritable> {
-	public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-	    try {
-		int sum = 0;
+        public void reduce(Text key, Iterable<IntWritable> values, Context context)
+                throws IOException, InterruptedException {
+            try {
+                int sum = 0;
 
-		// loop through different sales vales and add it to sum
-		for (IntWritable sales : values) {
-		    Integer intSales = new Integer(sales.toString());
-		    sum += intSales;
-		}
+                // loop through different sales values and add it to sum
+                for (IntWritable sales : values) {
+                    Integer intSales = new Integer(sales.toString());
+                    sum += intSales;
+                }
 
-		// create hbase put with rowkey as date
-		Put insHBase = new Put(key.getBytes());
+                // create hbase put with rowkey as date
+                Put insHBase = new Put(key.getBytes());
 
-		// insert sum value to hbase 
-		insHBase.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("sum"), Bytes.toBytes(sum));
+                // insert sum value to hbase
+                insHBase.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("sum"), Bytes.toBytes(sum));
 
-		// write data to Hbase table
-		context.write(null, insHBase);
-	    } catch (Exception e) {
-		e.printStackTrace();
-	    }
-	}
+                // write data to Hbase table
+                context.write(null, insHBase);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static void main(String[] args) throws Exception {
-	Configuration conf = HBaseConfiguration.create();
+        Configuration conf = HBaseConfiguration.create();
 
-	// define scan and define column families to scan
-	Scan scan = new Scan();
-	scan.addFamily(Bytes.toBytes("cf"));
-	Job job = Job.getInstance(conf);
-	job.setJarByClass(HBaseMapReduce.class);
+        // define scan and define column families to scan
+        Scan scan = new Scan();
+        scan.addFamily(Bytes.toBytes("cf"));
+        Job job = Job.getInstance(conf);
+        job.setJarByClass(HBaseMapReduce.class);
 
-	// define input hbase table
-	TableMapReduceUtil.initTableMapperJob("test1", scan, hbaseMapper.class, Text.class, IntWritable.class, job);
+        // define input hbase table
+        TableMapReduceUtil.initTableMapperJob("test1", scan, hbaseMapper.class, Text.class, IntWritable.class, job);
 
-	// define output table
-	TableMapReduceUtil.initTableReducerJob("test2", hbaseReducer.class, job);
-	job.waitForCompletion(true);
+        // define output table
+        TableMapReduceUtil.initTableReducerJob("test2", hbaseReducer.class, job);
+        job.waitForCompletion(true);
     }
 }
